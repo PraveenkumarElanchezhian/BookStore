7eb3a0eea30f58f5afe4ff022dc05ee75ea7b80e
PraveenkumarElanchezhian <epraveenkumar1997@gmail.com>
2022-02-10 00:26:49 +0530

    [Praveen]Validation added login page

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,18 +1,59 @@
 import React from 'react';
 import './Login.scss'
 import TextField from '@material-ui/core/TextField';
-import {Button} from '@material-ui/core';
+import { Button } from '@material-ui/core';
+import { Component } from 'react';
 
-function Login() {
-  return (
-    <div className="login-container">
+export class Login extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       EmailId:'',
+       Password:'',
+       EmailIdError: false,
+       PasswordError: false
+
+    }
+  }
+
+  validation =()=>{
+    let isError= false;
+    const error = this.state;
+    error.EmailIdError = this.state.EmailId === ''? true : false;
+    error.PasswordError= this.state.Password === '' ? true : false;
+
+    this.setState({
+        ...error
+    })
+    
+    return isError = error.EmailIdError || error.PasswordError ;
+
+}
+
+next=()=>{
+    var validated  = this.validation();
+    if(validated){
+        console.log("validation done")
+    }
+}
+
+changeHandle = (e) => {
+  this.setState({
+    [e.target.name]: e.target.value,
+  });
+};
+  
+  render() {
+    return (
+      <div className="login-container">
         <div className="Image-container">
           <div className="Image-left">
-          <img className="Image-dimension" src='https://public-v2links.adobecc.com/d096df37-ca37-4026-553f-8cfa6bec09ec/component?params=component_id%3A7b0eeb81-a918-4f41-9bc2-f508474e79ce&params=version%3A0&token=1644370240_da39a3ee_b43dfffc5713010c64d0af51433118d1fc13ba79&api_key=CometServer1'/>
-          <h4>ONLINE BOOK SHOPPING</h4>
+            <img className="Image-dimension" src='https://public-v2links.adobecc.com/d096df37-ca37-4026-553f-8cfa6bec09ec/component?params=component_id%3A7b0eeb81-a918-4f41-9bc2-f508474e79ce&params=version%3A0&token=1644370240_da39a3ee_b43dfffc5713010c64d0af51433118d1fc13ba79&api_key=CometServer1' />
+            <h4>ONLINE BOOK SHOPPING</h4>
           </div>
           <div className="Image-Right">
-            
+
           </div>
         </div>
 
@@ -24,12 +65,12 @@ function Login() {
           </div>
 
           <div className="section-2">
-            <TextField className="email-box"  type='text' id="outlined-email" label="Email Id" variant="outlined"/>
-            <TextField className="pass-box" style={{ backgroundColor: 'white' }} type='text' id="outlined-email" label="Password" variant="outlined" />
-          </div>   
+            <TextField  name="EmailId" className="email-box" type='text' id="outlined-email" label="Email Id" variant="outlined" error={this.state.EmailIdError} helperText={this.state.EmailIdError ? "Enter a correct email" : " "} onChange={(e) => this.changeHandle(e)}/>
+            <TextField name="Password" className="pass-box" style={{ backgroundColor: 'white' }} type='text' id="outlined-email" label="Password" variant="outlined" error={this.state.PasswordError} helperText={this.state.PasswordError ? "Enter a correct password" : " "} onChange={(e) => this.changeHandle(e)}/>
+          </div>
 
-          <button className='login-button' style={{ backgroundColor: '#A03037'}} > Login </button>
-          
+          <button className='login-button' style={{ backgroundColor: '#A03037' }} onClick={this.next}> Login </button>
+
           <p className='strick'>-----------OR-----------</p>
 
           <div className='buttom-button'>
@@ -38,10 +79,10 @@ function Login() {
           </div>
 
         </div>
-    </div>
-  )
+      </div>
+    )
+  }
 }
-
 export default Login;
 
 
--- src/pages/signup/Signup.jsx
+++ src/pages/signup/Signup.jsx
@@ -2,13 +2,19 @@ import React from 'react';
 import './Signup.scss'
 import TextField from '@material-ui/core/TextField';
 
-function Login() {
-  return (
-    <div className="signup-container">
+
+import { Component } from 'react';
+
+export class Signup extends Component {
+  render() {
+    return (
+      <div className="signup-container">
         <div className="signup-Image-container">
           <div className="signup-Image">
-          <img className="signup-Image-dimension" src='https://public-v2links.adobecc.com/d096df37-ca37-4026-553f-8cfa6bec09ec/component?params=component_id%3A7b0eeb81-a918-4f41-9bc2-f508474e79ce&params=version%3A0&token=1644370240_da39a3ee_b43dfffc5713010c64d0af51433118d1fc13ba79&api_key=CometServer1'/>
+            <img className="signup-Image-dimension" src='https://public-v2links.adobecc.com/d096df37-ca37-4026-553f-8cfa6bec09ec/component?params=component_id%3A7b0eeb81-a918-4f41-9bc2-f508474e79ce&params=version%3A0&token=1644370240_da39a3ee_b43dfffc5713010c64d0af51433118d1fc13ba79&api_key=CometServer1' />
+            <h4>ONLINE BOOK SHOPPING</h4>
           </div>
+          <div className='signup-Image-Right'></div>
         </div>
 
         <div className="signup">
@@ -19,23 +25,23 @@ function Login() {
           </div>
 
           <div className="signup-section-2">
-            <TextField className="email-npt" style={{ backgroundColor: 'white' }} type='text' id="outlined-email" label="FullName" variant="outlined"/>
+            <TextField className="email-npt" style={{ backgroundColor: 'white' }} type='text' id="outlined-email" label="FullName" variant="outlined" />
             <TextField className="pass-npt" style={{ backgroundColor: 'white' }} type='text' id="outlined-email" label="Email Id" variant="outlined" />
-          </div>   
+          </div>
 
           <div className="signup-section-3">
-            <TextField className="email-npt" style={{ backgroundColor: 'white' }} type='text' id="outlined-email" label="Password" variant="outlined"/>
+            <TextField className="email-npt" style={{ backgroundColor: 'white' }} type='text' id="outlined-email" label="Password" variant="outlined" />
             <TextField className="pass-npt" style={{ backgroundColor: 'white' }} type='text' id="outlined-email" label="Mobile Number" variant="outlined" />
-          </div>  
+          </div>
 
-          <button className='login-btn' style={{ backgroundColor: '#A03037'}} > Signup </button>
+          <button className='login-btn' style={{ backgroundColor: '#A03037' }} > Signup </button>
 
         </div>
-    </div>
-  )
+      </div>
+    )
+  }
 }
-
-export default Login;
+export default Signup;
 
 
 
